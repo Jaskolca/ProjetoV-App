950f7e19f7df897408dbe1c90669d9bdee70d19a
Jaskolca <jaskolcajazz@gmail.com>
2021-11-26 22:28:34 -0300

    fix: propositions

--- src/pages/Propositions/index.tsx
+++ src/pages/Propositions/index.tsx
@@ -16,28 +16,31 @@ const Propositions: React.FC = () => {
   const [propositions, setPropositions] = useState<IProposition[]>([]);
 
   useEffect(() => {
-    async function loadData {
-      const { data } = await api.get('propositions');
+    async function loadData() {
+      const { data } = await api.get("propositions");
 
       const { dados } = data;
 
       setPropositions(dados as IProposition[]);
     }
 
-    loadData()
+    loadData();
   }, []);
-  return <View>{
-    propositions.map((proposition, index) => (
-      <View key={`proposition-${index}`} style={{marginBottom: 20}}>
-        <Text>{proposition.ano}</Text>
-        <Text>{proposition.codTipo}</Text>
-        <Text>{proposition.ementa}</Text>
-        <Text>{proposition.id}</Text>
-        <Text>{proposition.numero}</Text>
-        <Text>{proposition.siglaTipo}</Text>
-        <Text>{proposition.uri}</Text>
-      </View>
-    ))}</View>;
+  return (
+    <View>
+      {propositions.map((proposition, index) => (
+        <View key={`proposition-${index}`} style={{ marginBottom: 20 }}>
+          <Text>{proposition.ano}</Text>
+          <Text>{proposition.codTipo}</Text>
+          <Text>{proposition.ementa}</Text>
+          <Text>{proposition.id}</Text>
+          <Text>{proposition.numero}</Text>
+          <Text>{proposition.siglaTipo}</Text>
+          <Text>{proposition.uri}</Text>
+        </View>
+      ))}
+    </View>
+  );
 };
 
 export default Propositions;
